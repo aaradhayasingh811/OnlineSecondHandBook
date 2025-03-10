9d37c0d1c7c33b76c848810a8c483867d4830329
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2025-03-10 18:31:44 +0530

    logout user

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -359,8 +359,8 @@ const logoutController = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true, 
+      sameSite: "None", 
     });
 
     res.status(200).json({ success: true, message: "Logged out successfully" });
