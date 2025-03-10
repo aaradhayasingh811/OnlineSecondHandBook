a3418fce8cebbe0992c3dbbdce6a815d20a77ab5
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2025-03-10 18:21:20 +0530

    trying to solve authorization error

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -140,10 +140,11 @@ const registerController = async (req, res) => {
       token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
   
       // Set token in an HTTP-Only cookie
+     
       res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        secure: true, 
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
   
